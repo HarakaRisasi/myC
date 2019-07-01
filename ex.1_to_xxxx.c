1fbd10fec8ee84765947f0ff4be85fe40b3b52d5
HarakaRisasi <harakarisasi@gmail.com>
2019-07-01 22:48:19 +1000

    add.ex.119

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -2005,6 +2005,31 @@
 	return 0;
     }
 
+//ex.119
+    //inch to mm.
+    //1 inch = 2.54 cm
+    int main(){
+    	float i, c; // inch range, centimeter
+    	float s; // step
+	float cm, inch, mm;
+	
+	int x; // to calculate
+	
+	printf("Inch range(0 to ...) = ");
+	scanf("%f", &i);
+	printf("Step = ");
+	scanf("%f", &s);
+	
+	
+	for(inch = 0; inch < i; inch = inch + s){    
+	    cm = inch * 2.54;    
+	    mm = cm * 10;
+	    
+	    printf("Inch = %.2f || mm = %.2f\n", inch, mm);
+	}
+	return 0;
+    }
+
 //ex.134
 
 //ex.260
