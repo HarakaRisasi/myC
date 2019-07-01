ff36135cc5bf36ac7e45e966e7fecfe239f57b00
HarakaRisasi <harakarisasi@gmail.com>
2019-07-01 22:45:24 +1000

    main

---
+++ test.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<sys/cdefs.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<ctime>
+
+//Logarithm - это операция обратная возведению в степен
+
+//inch to mm.
+//1 inch = 2.54 cm
+int main(){
+    float i, c; // inch range, centimeter
+    float s; // step
+    float cm, inch, mm;
+
+    int x; // to calculate
+
+    printf("Inch range(0 to ...) = ");
+    scanf("%f", &i);
+    printf("Step = ");
+    scanf("%f", &s);
+
+
+    for(inch = 0; inch < i; inch = inch + s){    
+	cm = inch * 2.54;
+    	mm = cm * 10;
+
+	printf("Inch = %.2f || mm = %.2f\n", inch, mm);
+    }
+    return 0;
+}
+
+	
+		
+
+
