b7e89b2c5f5ace0b9206aef091953708c2dc43f7
HarakaRisasi <harakarisasi@gmail.com>
2019-07-04 03:07:52 +1000

    add_ex.132

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -2335,5 +2335,27 @@
 	return 0;
     }
 
+//x.132
+    //Price and weght range by 100 - n(gr)
+    //100 gr = 1.65
+    //200 gr = 3.30
+    //... gr = n
+    int main(){
+	int w, mw;// w - weight(gramm); max.weight(gramm);
+	float p, sum;// p - price(100 gramm);
+	
+	printf("Price 100(gr) = ");
+	scanf("%f", &p);
+	printf("Max.weight(gr) = ");
+	scanf("%i", &mw);
+	
+	for(w; w <= mw; w += 100){
+	    sum = p * (w / 100);
+	    printf("Weight = %i || Price = %.2f\n", w, sum);
+	}
+	
+	return 0;
+    }
+
 //ex.144
 //ex.260
