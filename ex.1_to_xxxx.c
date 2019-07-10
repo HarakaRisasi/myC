b8e370689fc1efa6cb6eabe2ec19e747c426fed2
HarakaRisasi <harakarisasi@gmail.com>
2019-07-11 04:55:54 +1000

    add_ex.133

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -2335,7 +2335,7 @@
 	return 0;
     }
 
-//x.132
+//ex.132
     //Price and weght range by 100 - n(gr)
     //100 gr = 1.65
     //200 gr = 3.30
@@ -2357,5 +2357,34 @@
 	return 0;
     }
 
-//ex.144
+//ex.133
+    #define LB -4 //low range level the change of argument
+    #define HB 4 //high range level the chengae of argument
+    #define DX 0.5 //argument increment
+
+    //define - Preprocessor directive.
+    //         With it, you can create so-called named constants.
+    /*         С директивой define препроцессор работает следующим образом. 
+    	       В тексте программы находятся все места, где встречается имя, 
+	       указанное в директиве define. 
+	       После этого вместо этого имени подставляется значение, указанное в 
+	       директиве define.
+     */
+    // define {name}{value};
+    int main(){
+	float x, y; //argument; value of function
+	int n; //dots amount
+	int i; //loops counter
+	
+	printf("\nTable the function values y = |x| \n");
+	n = (HB - LB) / DX + 1;
+	x = LB;
+	for(i = 1; i <= n; i++){
+	    y = abs(x); //abs - absolute value calculation
+	    printf("%.2f %.2f\n", x, y);
+	    x += DX;
+	}   
+    }  
+
+//ex.149
 //ex.260
