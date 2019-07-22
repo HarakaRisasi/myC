9cec1c84d41ef8be28a97aa309864aa038322b67
HarakaRisasi <harakarisasi@gmail.com>
2019-07-22 18:09:32 +1000

    ?template to console: sys.(clear); getchar(); sys.(clear);?

--- test.c
+++ test.c
@@ -1,50 +1,30 @@
-#include<stdio.h>
 #include<sys/cdefs.h>
-#include<stdlib.h>
 #include<math.h>
 #include<string.h>
 #include<time.h>
 #include<stdbool.h>
-
-//Logarithm - это операция обратная возведению в степень.
-
+#include<stdio.h> //for getchar();
+#include<stdlib.h> //for system("");
+#include <unistd.h> //for sleep();
+//output value of function
+//y = -2.4x² + 5x - 3;
+//range x: -2 >>> 2;
 int main(){
-    int x, y, i, ans, error = 0, t_er = 0;
-    float amount = 0, mark = 0, z = 0;
-
-    for(x = 1; x < 10; x++){
-	printf("\nTable #%i\n", x);
-    	for(i = 1; i < 10; i++){
-    	    y = x * i;
-
-	    printf("%i * %i = ", x, i);
-	    scanf("%i", &ans);
-	    if(ans != y){
-		printf("NO! Right is %i\n", y);
-		error++;
-		t_er++;
-	    }	    
-	}
-	if(error > 0){
-	    printf("\nThe number of errors in the table #%i is %i", x, error);
-	    if(error == 1)
-		mark += 4;
-	    else if(error == 2 || error == 3)
-		mark += 3;
-	    else
-		mark += 1;	    
-	}else
-	    mark += 5;
-	amount++;
-
-	error = 0;	    
-
-	printf("\n");
+    system("clear");
+    int i = 1;
+    float y, x;
+    x = -2.0;
+    
+    printf("     X        Y\n");
+    while(x <= 2){
+    	y = -2.4 * x*x + 5 * x - 3;
+    	printf("#%i > %.2f    %.2f\n",i++, x, y);
+	sleep(1);
+	x += 0.5;
+	
     }
-    printf("Total error: %i\n", t_er);
-
-    z = mark / amount;
-
-    printf("Average mark: %.2f\n", z);
+  
+    getchar(); 
+    system("clear");
+    return 0;
 }
-
