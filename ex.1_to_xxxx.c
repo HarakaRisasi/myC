4a3e458681d63e6326f483296382e07b59b13aba
HarakaRisasi <harakarisasi@gmail.com>
2019-07-26 04:01:56 +1000

    add_ex.152

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -2976,5 +2976,34 @@
 //ex.151
 
 //ex.152
-    
+    //Guess the number 1 to 10;
+    int main(){
+    	system("clear");
+    	
+    	int i = 1, x, count, ans;
+	
+	srand(time(NULL));  
+	x = 1 + rand() % 10;
+	printf("The computer 'conceived' a number from 1 to 10.\n");    
+	printf("Enter  number: \n");
+	
+	do{
+	    printf("%i/5 > ", i);
+	    scanf("%i", &ans);
+	    
+	    if(ans == x){               
+		printf("YES! You Right it's: %i\n", x);
+		break;      
+	    }else       
+		count++;    
+	    
+	    i++;
+	}    
+	while(count < 5);
+	
+	return 0;
+    }
+
+//ex.153
+
 //ex.260
