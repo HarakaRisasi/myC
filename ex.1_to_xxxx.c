dc93aada62a9db098e541f4de4c2dab9cf0be9c4
HarakaRisasi <harakarisasi@gmail.com>
2019-07-29 04:54:43 +1000

    add_ex.156

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -3,6 +3,7 @@
 //ex.139
 //ex.151
 //ex.153
+//ex.155
 
 #include<stdio.h>
 
@@ -3032,5 +3033,46 @@
 	
 	return 0;
     }
+
+//ex.155
+
+//ex.156
+    //Greatest Common Divider (GCD)
+    int main(){
+	system("clear");
+	int nod, num_x, num_y , x, y;    
+	
+	printf("X = "); scanf("%i", &x);
+	
+	printf("Y = "); scanf("%i", &y);
+	
+	//check: X > Y
+	if(x > y){
+	    num_x = x; 
+	    num_y = y;
+	}else{
+	    num_x = y;
+	    num_y = x;
+	}
+	
+	//Из большего числа вычитаем меньшее.
+	//Если получается 0,то значит,что числа равны друг другу и являются НОД 
+	//  (следует выйти из цикла).
+	//Если результат вычитания не равен 0, то большее число заменяем на 
+	//  результат вычитания.
+	//Переходим к пункту 1.
+	while(num_y != 0){
+	    if(num_x % num_y == 0){
+		printf("END nod is %i\n", num_y);
+		break;
+	    }else
+		num_y = num_x % num_y;    
+	}   
+	
+	return 0;
+    }
+
+//ex.157
+    
 
 //ex.260
