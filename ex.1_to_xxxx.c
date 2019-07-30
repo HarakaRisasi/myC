1c72a70d18a627f163299d2f787b51ff7be69bf5
HarakaRisasi <harakarisasi@gmail.com>
2019-07-31 03:51:47 +1000

    add_ex.159

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -3176,7 +3176,7 @@
      */
 
     //Solution:
-    //!define spave of array
+    //!define space of array
     #define AMOUNT 6 //the array's space.
     int main(){
 	system("clear");
@@ -3211,4 +3211,59 @@
 	return 0;
     }
 
+//ex.159
+    //Condition:
+    //-> 23 0 45 -5 12
+    //Min. element of array: -5
+
+    //Slution:
+    //!define space of array
+    #define AMO 4 //the array's space.
+    int main(){
+    	system("clear");
+	
+	int a[AMO],i, n, min;
+	
+	while(i < AMO){
+	    printf("%d / %d >> ", i + 1, AMO);
+	    scanf("%d", &a[i]);
+	    i++;
+	    system("clear");
+	}
+	
+	i = 0;
+	
+	min = 0;//Присвоить 0-й ячейке массива, статус минимального числа
+	/*
+	   !!! Важно.
+	   При работе с элементами массива, использовать специфику синтаксиса,
+	   свойственную только работе внутри массива.
+	   
+	   Пример:
+	   Без массива, эта же задача выглядела бы так:
+	   min = n;
+	   if(min > n)
+	   min = n; 
+	   
+	 */
+	
+	for(i = 1; i < AMO; i++){ //i = 1; так как min = 0;
+	    //тоесть a[0] начнет сравниваться с a[1];
+	    
+	    if(a[min] > a[i])//if TRUE
+		min = i;//than a[0] apropriate (i) or a[i]
+	    //example:
+	    //if   a[min = 0] = 4 && a[i = 1] = 3
+	    //than a[min = 1] = 3
+	}
+	
+	printf("Min array's element: %d\n", a[min]);
+	
+	return 0;
+    }
+	
+	
 //ex.260
+	
+	
+
