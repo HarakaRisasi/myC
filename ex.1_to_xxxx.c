de7fad91fe5fff245acd191b5ab71625707f043e
HarakaRisasi <harakarisasi@gmail.com>
2019-07-31 04:22:45 +1000

    add_ex.160

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -3261,9 +3261,49 @@
 	
 	return 0;
     }
+
+//ex.160
+    //Condition:
+    //-> 23 0 45 -5 12
+    //Min. element of array: -5
+    //!Use Pointers to access to elements
+
+    //Slution:
+    //!define space of array
+    #define AMO 4 //the array's space.
+    int main(){
+	system("clear");
 	
+	int a[AMO]; //array;
+	int *min; //number min. element;
+	int *p; //array's element pointer;
+	int i;
 	
+	p = a;
+	
+	for(i = 1; i <= AMO; i++){
+	    printf(">> ");
+	    scanf("%d", p++);
+	    system("clear");
+	}
+	for(i = 0; i < AMO; i++){
+	    printf("-> %d;\n", a[i]);
+	}
+	
+	min = a; //min = a[0];
+	p = a + 1;    
+	
+	for(i = 2; i <= AMO; i++){
+	    if(*p < *min) min = p;
+	    p++;
+	}
+	printf("\nMin element of array: %d\n", *min);
+    }
+
+//ex.161
+
+
 //ex.260
-	
-	
+
+
 
