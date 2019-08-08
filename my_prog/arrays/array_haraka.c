a4f8d8244773273c93e5d2d84c66b48581858422
HarakaRisasi <harakarisasi@gmail.com>
2019-08-08 18:03:17 +1000

    mv_to_arrays_folders

---
+++ my_prog/arrays/array_haraka.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    
+    system("clear");
+
+    char alpha[] = {72, 97, 114, 97, 107, 97};
+    int i, j;
+    for(i = i; i < 6; i++){
+	
+	printf("%c ", alpha[i]);
+    }
+    printf("\n");
+}
