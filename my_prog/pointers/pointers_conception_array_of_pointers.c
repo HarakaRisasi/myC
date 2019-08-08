a8115c3dcd6e0dcd473164df4142e57f81a44491
HarakaRisasi <harakarisasi@gmail.com>
2019-08-08 18:05:01 +1000

    refactoring_master_directory

--- my_prog/pointers/pointers_conception_array_of_pointers.c
+++ my_prog/pointers/pointers_conception_array_of_pointers.c
@@ -5,7 +5,7 @@
 int main(){
     
     system("clear");
-/*
+    
     int var[] = {11, 100, 200};
     int i;
 
@@ -14,14 +14,4 @@ int main(){
     }
 
     return 0;
-}
-*/
-    char a_char[N];
-    int i;
-
-    scanf("%s", a_char);
-
-    for(i = 0; i < N; i++)
-        printf("symbol %c = ASCII #%i\n",a_char[i], a_char[i]);
-    
 }
