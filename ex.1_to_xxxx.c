63f7260085cebb4ac282ac406ec6ba4d2a833653
HarakaRisasi <harakarisasi@gmail.com>
2019-08-22 22:23:32 +1000

    ?middle_weak_weather?

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -38,7 +38,7 @@
 
 //ex.14
 	//y = -2.7x³+0.23x² - 1.4
-	    int x = n;
+	int x = n;
 	float y = -2.7* x*x*x + 0.23* x*x - 1.4
 
 //ex.15
@@ -3366,7 +3366,7 @@
 	int arr[NUM];
 	int *ptr[NUM];
 	int min, max;
-	int i, j;
+	int i;
 	int sum = 0;
 	float count = -2;
 	float avg;
@@ -3405,8 +3405,58 @@
     }
 
 //ex.163
+    //condition:
+    /*
+       Sun >> 1
+       Mon >> 2
+       Tue >> 3
+       Wed >> 4
+       Thu >> 5
+       Fri >> 6
+       Sat >> 7
+       -------------------------------
+       Avg. weather: 4.00
+     */
+
+    //char arr[NUMBER_OF_STRING][MAX_STRING_SIZE]
+
+    #define NUM 7
+    int main(){
+	system("clear");
+	
+	//Initialization
+	int arr[NUM];
+	char weak[NUM][5] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+	int *ptr[NUM];
+	int i;
+	float avg, sum = 0;
+	int count = 0;
+	
+	//Calculation    
+	printf("Enter %d num.(no less than 3)\n", NUM);
+	for(i = 0; i < NUM; i++){
+	    scanf("%d", &arr[i]);
+	    ptr[i] = &arr[i];
+	    sum += *ptr[i];
+	    count++;
+	}
+	
+	avg = sum / count;
+	
+	//Output
+	system("clear");
+	
+	for(i = 0; i < NUM; i++){
+	    printf("%s >> ", weak[i]);
+	    printf("%d\n", *ptr[i]);
+	}
+	
+	printf("-------------------------------");
+	printf("\nAvg. weather: %.2f\n", avg);
+    }
+
+//ex.164
+
+//ex.172
 
 //ex.260
-
-
-
