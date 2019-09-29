916f460ae339eb678ec1ac59538bb18346acdb4d
HarakaRisasi <harakarisasi@gmail.com>
2019-09-30 03:08:12 +1000

    add.ex_169

--- ex.1_to_xxxx.c
+++ ex.1_to_xxxx.c
@@ -3757,6 +3757,60 @@
 	return 0;
     }
 
+//ex.169
+    //condition:
+    /*
+    Input:
+    a[0] = 1
+    a[1] = 3
+    a[2] = 2
+    a[3] = 4
+    a[4] = 21
+    a[5] = 6
+    a[6] = 5
+    a[7] = 3
+    a[8] = 2
+    a[9] = 4
+    
+    Output:
+    1 2 2 3 3 4 4 5 6 21 
+    */
+
+    #define NUM 10
+
+    void bubbleSort(int *num, int size){
+    	for(int i = 0; i < size - 1; i++){
+	    for(int j = (size - 1); j > i; j--){
+		if(num[j - 1] > num[j]){
+		    int temp = num[j - 1];
+		    num[j - 1] = num[j];
+		    num[j] = temp;
+		}
+	    }
+	}
+    }
+
+    int main(){
+    	system("clear");
+    	
+	int i;
+	int a[NUM];
+	
+	//Ввод значения элементов массива
+	for(i = 0; i < NUM; i++){
+	    printf("a[%d] = ", i);
+	    scanf("%d", &a[i]);
+	}
+	bubbleSort(a, 10); //Вызов функции сортировки
+	
+	//Вывод отсортированных элементов массива
+	for(i = 0; i < NUM; i++)
+	    printf("%d ", a[i]);
+	
+	printf("\n");
+	
+	return 0;
+    }
 
 //ex.172
 
